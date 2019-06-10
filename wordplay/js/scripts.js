aecbb6cb06d400d6207016ffdf5f6e5d0f050366
micah zaayer and Tae Lee <micah@>
2019-06-10 16:32:52 -0700

    add new array that pulls all strings greater than 3 characters.

---
+++ wordplay/js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $(".user").submit(function(event) {
+    var userInput = $("#sentence").val();
+    wordArray = userInput.split(' ');
+    console.log(wordArray);
+    var filteredArray = [];
+    // for (i = 0; wordArray[i].length > 3; i++) {
+    //   filteredArray.push(wordArray[i]);
+    // }
+    wordArray.forEach(function(word) {
+      if(word.length > 3) {
+        filteredArray.push(word);
+      }
+    });
+    console.log(filteredArray);
+    event.preventDefault();
+  });
+});
